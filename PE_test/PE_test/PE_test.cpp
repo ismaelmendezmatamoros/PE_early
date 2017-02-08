54f17f4bb8b61d27fb187c3159b85cf52e5c381c
Eicke <ismael.mendez.matamoros@gmail.com>
2017-02-08 19:15:47 +0100

    task

--- PE_test/PE_test/PE_test.cpp
+++ PE_test/PE_test/PE_test.cpp
@@ -1,11 +1,23 @@
 // PE_test.cpp: define el punto de entrada de la aplicación de consola.
 //
 
-#include "stdafx.h"
+//#include "stdafx.h"
+#include<iostream>
+#include<vector>
+#include"PEScheduledFunctionTask.h"
+#include<string>
 
+int de(int h) {
+	std::cout << "cac" << std::endl;
+	return  8;
+}
 
 int main()
 {
+	std::string h("caca");
+	PEScheduledFunctionTask p(0, 0, 0);
+	p.setFunction<int, int>(de);
+
     return 0;
 }
 
